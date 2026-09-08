b22761af83f2f41ef5bd96997c1c3b42ab1ca66f
Joao Watanabe <jvwatanabe@gmail.com>
2026-09-08 13:04:19 -0300

    feat: implementa funcionalidades de frear e imprimir dados do veiculo (requisitos 3 e 5)

--- index.ts
+++ index.ts
@@ -23,7 +23,12 @@ while(true){
         case 1:
             acelerar(carro);
             break;
-    
+        case 2:
+            frear(carro);
+            break;
+        case 5:
+            imprimirDados(carro);
+            break;
         default:
             break;
     }
@@ -36,6 +41,20 @@ function acelerar(veiculo: Veiculo): void{
     veiculo.velocidade += veiculo.potencia*0.1;
     console.log(veiculo.velocidade);
 }}
+
+function frear(veiculo: Veiculo): void {
+    if (veiculo.velocidade > 0) {
+        veiculo.velocidade -= veiculo.potencia * 0.1;
+        if (veiculo.velocidade < 0) {
+            veiculo.velocidade = 0;
+        }
+    }
+    console.log(veiculo.velocidade);
+}
+
+function imprimirDados(veiculo: Veiculo): void {
+    console.table(veiculo);
+}
 
 function criaVeiculo(): Veiculo{
     const veiculo: Veiculo = new Veiculo();
